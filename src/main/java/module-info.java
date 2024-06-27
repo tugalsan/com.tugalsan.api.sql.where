2b2fc291eae7f752c0ab117323dc4fd667b24fb8
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 11:59:46 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module com.tugalsan.api.sql.where {
     requires java.sql;
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.string;
